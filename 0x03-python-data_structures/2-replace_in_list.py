0dd854a6c7b8fabc7407d1fdb263a2673ec8c527
frank4wealth <frank4wealth@DESKTOP-T2DIPHK.localdomain>
2022-07-03 19:39:47 +0100

    All Task

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,9 +2,11 @@
 
 def replace_in_list(my_list, idx, element):
 
-    if idx < 0 or idx >= len(my_list): #idx is negative or idx is out of range
+    if idx < 0:
+        return my_list
+
+    if idx >= len(my_list):
         return my_list
 
     my_list[idx] = element
     return my_list
-
