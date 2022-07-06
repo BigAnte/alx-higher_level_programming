5bb49c64eda7449573ac259cb15cd0687fdb6894
frank4wealth <frank4wealth@DESKTOP-T2DIPHK.localdomain>
2022-07-06 18:42:12 +0100

    All Task

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-
 def multiply_by_2(a_dictionary):
+    new_dir = a_dictionary.copy()
+    list_keys = list(new_dir.keys())
 
-    new_dict = {}
+    for i in list_keys:
+        new_dir[i] *= 2
 
-    for k, v in a_dictionary.items():
-
-        new_dict[k] = v * 2
-
-    return new_dict
+    return (new_dir)
